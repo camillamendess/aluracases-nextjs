06a4bf1366fd42134ae869e1c6bd0f553bd5e818
Camilla <milla_mendes83@hotmail.com>
2024-10-17 09:40:06 -0300

    Testando aplicação de estilos

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
+import React from 'react';
 import Link from '../src/components/Link';
+
+function Title({ children }) {
+  return (
+    <React.Fragment>
+      <h1>{children}</h1>
+      <style jsx>{`
+        h1 {
+          color: red;
+        }
+      `}</style>
+    </React.Fragment>
+  );
+}
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Alura Cases - Home</h1>
+      <Title>Alura Cases - Home</Title>
       <Link href="/faq">Ir para a página do FAQ</Link>
     </div>
-  )
+  );
 }
